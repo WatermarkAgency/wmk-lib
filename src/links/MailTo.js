ad67af4ae8719097489afcc6607585e61fd2656e
Peter Washington <peter@watermarkadvertising.net>
2020-04-02 14:05:03 -0600

    typo fixing

--- src/links/MailTo.js
+++ src/links/MailTo.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const MailTo = ({ className, id, children }) => {
-    const classes = {className}
-    classes.push("wmk-link");
-    classes.push("wmk-link-mailto");
+  const classes = [className];
+  classes.push("wmk-link");
+  classes.push("wmk-link-mailto");
   return (
     <a className={classes.join(" ")} id={id} href={"mailto:" + children}>
       {children}
@@ -16,7 +16,7 @@ export default MailTo;
 
 MailTo.propTypes = {
   id: PropTypes.string,
-  className: propTypes.string
+  className: PropTypes.string
 };
 
 MailTo.defaultTypes = {
