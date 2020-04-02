6683e810fed6fbb15a89e570da8b41c394f5442b
Peter Washington <peter@watermarkadvertising.net>
2020-04-02 14:17:21 -0600

    added Tel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import LinkMailTo from './links/MailTo'
+import LinkTel from './links/Tel'
 
+/* Link Components */
 export const MailTo = LinkMailTo
+export const Tel = LinkTel
 
-export default () => {
-  <h1>WMK Components Library</h1>
-}
+export default () => <h1>WMK Components Library</h1>
---
+++ src/links/Tel.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Tel = ({ className, id, children }) => {
+  const classes = [className];
+  classes.push("wmk-link");
+  classes.push("wmk-link-tel");
+  return (
+    <a
+      className={className}
+      id={id}
+      href={"tel:" + children.replace(/\D/g, "")}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Tel;
+
+Tel.propTypes = {
+  classes: PropTypes.string,
+  id: PropTypes.string
+};
+
+Tel.defaultTypes = {
+  id: "",
+  className: ""
+};
