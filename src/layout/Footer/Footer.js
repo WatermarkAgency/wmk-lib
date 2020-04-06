bec784d1a2cc62157fc106397a406dc8358f224e
Peter Washington <peter@watermarkadvertising.net>
2020-04-06 11:24:17 -0600

    1.5.1-No hardcoded Copyright

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { wmkClass, Copyright } from "wmk-lib";
+import { wmkClass } from "wmk-lib";
 
 const Footer = ({ children, className, id }) => {
   return (
     <footer class={wmkClass("footer", "layout", className)} id={id}>
       {children}
-      <Copyright>Copyright goes here.</Copyright>
     </footer>
   );
 };
