eaa89838aefbda1fd2406eefe8bcd1042c389c29
Peter Washington <peter@watermarkadvertising.net>
2020-04-09 16:50:13 -0600

    1.7.2-Forgot the exports again

--- src/index.js
+++ src/index.js
@@ -3,39 +3,41 @@ import LinkMailTo from "./links/MailTo";
 import LinkTel from "./links/Tel";
 import LinkAnchor from "./links/Anchor";
 import LinkSocialMediaIcons from "./links/SocialMediaIcons/SocialMediaIcons";
-import LinkWMKLink from "./links/WMKLink"
+import LinkWMKLink from "./links/WMKLink";
 
 import LayoutCopyright from "./layout/Footer/Copyright";
 import LayoutFlexSpacer from "./layout/FlexSpacer/FlexSpacer";
-import LayoutFooter from './layout/Footer'
+import LayoutFooter from "./layout/Footer";
 import LayoutMainLayout from "./layout/MainLayout";
+import LayoutSticky from "./layout/Sticky/Sticky";
+import LayoutHeader from "./layout/Header/Header";
 
 import MediaVideo from "./media/Video/Video";
 
-import MenusMenuItems from './menus/MenuItems/MenuItems'
+import MenusMenuItems from "./menus/MenuItems/MenuItems";
 
 import ContentfulLogo from "./cms/Logo/CtflLogo";
-
 
 /* Layout Components */
 export const Copyright = LayoutCopyright;
 export const MainLayout = LayoutMainLayout;
 export const FlexSpacer = LayoutFlexSpacer;
 export const Footer = LayoutFooter;
-
+export const Header = LayoutHeader;
+export const Sticky = LayoutSticky;
 
 /* Link Components */
 export const MailTo = LinkMailTo;
 export const Tel = LinkTel;
 export const Anchor = LinkAnchor;
 export const SocialMediaIcons = LinkSocialMediaIcons;
-export const WMKLink = LinkWMKLink
+export const WMKLink = LinkWMKLink;
 
 /* Media Components */
 export const Video = MediaVideo;
 
 /* Menus */
-export const MenuItems = MenusMenuItems
+export const MenuItems = MenusMenuItems;
 
 export const CtflLogo = ContentfulLogo;
 
