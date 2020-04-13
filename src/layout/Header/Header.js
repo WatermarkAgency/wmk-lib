5f7246633e40cb3ab54e1d7c8d202c7515650daa
Peter Washington <peter@watermarkadvertising.net>
2020-04-13 10:07:35 -0600

    1.7.4-Sticky/Header wrappers in but not functional

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { wmkClass } from "../../logic";
 
 const Header = ({ children, className, offset, style }) => {
-  style.top = offset;
+  //   style["top"] = offset ? offset : 0;
   return (
     <header className={wmkClass("header", "layout", className)} style={style}>
       {children}
