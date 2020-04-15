4dfebb330a0e4a394c23ac506cdc5ccbf866cfd9
Peter Washington <peter@wmkagency.com>
2020-04-15 16:23:14 -0600

    1.10.5-changed import path to hopefully fix styles

--- src/layout/FlexSpacer/FlexSpacer.js
+++ src/layout/FlexSpacer/FlexSpacer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { wmkClass } from '../../logic';
 import PropTypes from 'prop-types'
-import "./FlexSpacer.css"
+import "./dist/FlexSpacer.css"
 
 const FlexSpacer = ({ className, id }) => {
   return <div className={wmkClass('flex-spacer','layout',className)} id={id} />;
--- src/loader/Spinner/Spinner.js
+++ src/loader/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Spinner.css";
+import "./dist/Spinner.css";
 
 const Spinner = () => (
   <div
