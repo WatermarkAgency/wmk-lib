20e64e8a1dad5c5029766a315fd6500a938d4ec1
Peter Washington <peter@wmkagency.com>
2020-04-16 15:39:32 -0600

    1.11.0-Sticky header initial component

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { wmkClass } from "../../logic";
 
-const Header = ({ children, className, offset, style }) => {
+const Header = React.forwardRef(({ children, className, style }, ref) => {
   //   style["top"] = offset ? offset : 0;
   return (
-    <header className={wmkClass("header", "layout", className)} style={style}>
+    <header
+      className={wmkClass("header", "layout", className)}
+      style={style}
+      ref={ref}
+    >
       {children}
     </header>
   );
-};
+});
 
 export default Header;
--- src/layout/Sticky/Sticky.js
+++ src/layout/Sticky/Sticky.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { wmkClass } from "../../logic";
 import PropTypes from "prop-types";
 import Header from '../Header/Header'
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
 
-const StickyHeader = ({ Alert, offset, className, children }) => {
+const StickyHeader = ({ Alert, className, children, absolute }) => {
+  const domPosition = absolute ? "absolute" : "relative";
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [scrollPos, setScrollPos] = useState(0);
+
+  const headerRef = useRef();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setHeaderHeight(headerRef.current.getBoundingClientRect().height);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [headerHeight]);
+
+  useScrollPosition(({ currPos }) => {
+    setScrollPos(currPos.y * -1);
+  });
   return (
-    <div className={wmkClass("sticky-header", "layout", className)}>
+    <div className={wmkClass("sticky-header", "layout", className)}
+    style={{ position: scrollPos > headerHeight ? "fixed" : domPosition }}>
       {Alert}
-      <Header offset={offset}>{children}</Header>
+      <Header ref={headerRef}>{children}</Header>
+      {scrollPos > headerHeight && <div style={{ height: headerHeight }} />}
     </div>
   );
 };
@@ -16,13 +37,13 @@ export default StickyHeader;
 
 StickyHeader.propTypes = {
   Alert: PropTypes.node,
-  offset: PropTypes.string,
   className: PropTypes.string,
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
+  absolute: PropTypes.bool
 };
 
 StickyHeader.defaultProps = {
   Alert: <React.Fragment />,
-  offset: 0,
-  className: ""
+  className: "",
+  absolute: false
 };
