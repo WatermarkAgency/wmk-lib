81c7e7c24486ac0e484ef29ef1ec5cdba0ba674e
Peter Washington <peter@wmkagency.com>
2020-05-11 17:25:00 -0600

    1.12.12-Automatic video responsiveness to square ratio

--- src/media/Video/Video.js
+++ src/media/Video/Video.js
@@ -8,43 +8,57 @@ import "./Video.css";
 
 const Video = ({ id, className, url, poster, dimensions, Loader }) => {
   const [readyState, setReadyState] = useState(null);
-  const [hasLoaded, setHasLoaded] = useState(false)
-  //const viz = !readyState || readyState < 4 ? "hidden" : "visible";
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [winWidth, setWinWidth] = useState(0);
 
-  const playerRef = useRef()
+  const playerRef = useRef();
   useEffect(() => {
-    playerRef.current.subscribeToStateChange((state,prevState)=>{
-      setReadyState(state.readyState)
-      if(state.readyState > 3) setHasLoaded(true)
-    })
+    playerRef.current.subscribeToStateChange((state, prevState) => {
+      setReadyState(state.readyState);
+      if (state.readyState > 3) setHasLoaded(true);
+    });
+    const handleResize = () => {
+      setWinWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   });
-  const _id = id === "" ? false : id
+
+  const edgeLong = dimensions.split("x")[0];
+  const edgeShort = dimensions.split("x")[1];
   return (
-    <div id={_id} 
-      className={wmkClass("video", "media", className)} 
-      style={{position: 'relative'}}
-    > 
+    <div
+      id={id}
+      className={wmkClass("video", "media", className)}
+      style={{ position: "relative" }}
+    >
       {/*This image sets the aspect ratio of the video*/}
-      <img src={`https://via.placeholder.com/${dimensions}.jpg`} alt=""
-      style={{
-        width: '100%',
-        maxWidth: 'none',
-        visibility: 'hidden'
-      }}
+      <img
+        src={`https://via.placeholder.com/${
+          winWidth > edgeShort
+            ? `${edgeLong}x${edgeShort}`
+            : `${edgeShort}x${edgeShort}`
+        }.jpg`}
+        alt=""
+        style={{
+          width: "100%",
+          maxWidth: "none",
+          visibility: "hidden"
+        }}
       />
       {(!readyState || readyState < 4) && !hasLoaded ? <Loader /> : null}
-        <Player
-          ref={playerRef}
-          poster={poster}
-          preload="auto"
-          muted={true}
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
-        >
-          <source src={url} />
-          <ControlBar disableCompletely={true} />
-        </Player>
+      <Player
+        ref={playerRef}
+        poster={poster}
+        preload="auto"
+        muted={true}
+        autoPlay={true}
+        loop={true}
+        playsInline={true}
+      >
+        <source src={url} />
+        <ControlBar disableCompletely={true} />
+      </Player>
     </div>
   );
 };
@@ -53,17 +67,17 @@ export default Video;
 
 Video.propTypes = {
   url: PropTypes.string,
-  id: PropTypes.string,
+  id: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   poster: PropTypes.string,
   dimensions: PropTypes.string,
   Loader: PropTypes.func
 };
 
 Video.defaultProps = {
-  id: "",
+  id: false,
   url:
     "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_5MB.mp4",
   poster: "",
-  dimensions: '1280x720',
+  dimensions: "1280x720",
   Loader: Loading
 };
