0797b9689fa98dc1ca6b1dd7869adc48928e4428
Peter Washington <peter@wmkagency.com>
2021-08-25 08:58:39 -0600

    added header component from v1

--- src/layout/Sticky.jsx
+++ src/layout/Sticky.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useScrollPosition } from "@n8tb1t/use-scroll-position";
-import { Header } from "../Header/Header";
 import { wmkClass } from "../logic";
+import { Header } from "./Header";
 
 export const Sticky = ({
   Alert,
