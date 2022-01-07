2e027f04509c485625424b98bba6e199100785ec
Kennan Smith <kennan@watermarkadvertising.net>
2022-01-07 14:40:42 -0700

    2.2.5.2 - set up the width detection and storing;

--- src/ui/Slider/Slider.jsx
+++ src/ui/Slider/Slider.jsx
@@ -1,12 +1,24 @@
 import * as React from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import "./slider.css";
 
 export const Slider = ({ settings }) => {
   const { dots, infinite, speed, slidesToShow, slidesToScroll } = settings;
 
+  const [ sliderWidth, setSliderWidth ] = useState(0);
+  const sliderRef = useRef();
+  useEffect(() => {
+  const handleResize = () =>
+  setSliderWidth(sliderRef.current.getBoundingClientRect().width);
+  handleResize();
+  window.addEventListener('resize', handleResize);
+  console.log('sliderWidth: ', sliderWidth);
+  return () => window.removeEventListener('resize', handleResize);
+  }, [sliderWidth]);
+
   return (
-    <div className="wrap">
+    <div className="wrap" ref={sliderRef}>
       slider
     </div>
   );
