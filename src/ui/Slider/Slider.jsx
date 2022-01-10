459ae59ed411e13e6ca4475a2b6c0c4e577934c7
Kennan Smith <kennan@watermarkadvertising.net>
2022-01-10 14:37:28 -0700

    2.2.5.5 - at some point, just about everything about the slider just stopped working and I've been wrestling it back ever since - I finally got the sizing of the slider fixed, but I'm still working on getting the correct slide to show based on the currSlide index saved to state;

--- src/ui/Slider/Slider.jsx
+++ src/ui/Slider/Slider.jsx
@@ -3,50 +3,102 @@ import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import "./slider.css";
 
-export const Slider = ({ settings, SlideComponent, slidesDataArray }) => {
-  const { dots, infinite, speed, slidesToShow, slidesToScroll } = settings;
-
-  const [currSlide, setCurrSlide] = useState(0);
-  // detect width of slider
-  const [sliderWidth, setSliderWidth] = useState(0);
-  const sliderRef = useRef();
+export const Slider = ({
+  settings,
+  SlideComponent,
+  slidesDataArray,
+  arrowImageSrc
+}) => {
+  const { arrows, dots, speed, slidesToShow, slidesToScroll } = settings;
+  const safeSlidesToScroll =
+    slidesToScroll <= slidesToShow ? slidesToScroll : slidesToShow;
+  const prevNextButtonWidth = 80;
+  // detect width of wrap and slider window
+  const [wrapWidth, setWrapWidth] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(0);
+  const wrapRef = useRef();
+  const windowRef = useRef();
   useEffect(() => {
     const handleResize = () =>
-      setSliderWidth(sliderRef.current.getBoundingClientRect().width);
+      setWrapWidth(wrapRef.current.getBoundingClientRect().width);
+      // setWindowWidth(windowRef.current.getBoundingClientRect().width);
+      if (wrapWidth > 0) {
+        setWindowWidth(wrapWidth - 2*prevNextButtonWidth)
+      } 
     handleResize();
     window.addEventListener("resize", handleResize);
-    console.log("sliderWidth: ", sliderWidth);
+    console.log("wrapWidth: ", wrapWidth);
+    console.log("windowWidth: ", windowWidth);
     return () => window.removeEventListener("resize", handleResize);
-  }, [sliderWidth]);
-  // calculate how far left the slides container should be pulled to show the correct current slide(s)
+  }, [windowWidth, wrapWidth]);
+  // ===== calculate how far left the slides container should be pulled to show the correct current slide(s) =====
   const slidesValidated =
     slidesDataArray && Array.isArray(slidesDataArray) && slidesDataArray.length;
-  const slideWidth = sliderWidth / slidesToShow;
+  const slideWidth = windowWidth / slidesToShow;
   const slideCount = slidesValidated ? slidesDataArray.length : 0;
-  // If showing an even number of slides, the current slide will be the left slide of the middle two slides.
-  const evenOffset = (slidesToShow / 2) - 1;
+  // if showing an even number of slides, the current slide will be just left of center
+  const evenOffset = slidesToShow / 2 - 1;
   const evenLeft = -((currSlide - evenOffset) * slideWidth);
-  // If showing an odd number of slides, the current slide will be centered
+  // if showing an odd number of slides, the current slide will be centered
   const oddOffset = Math.floor(slidesToShow / 2);
   const oddLeft = -((currSlide - oddOffset) * slideWidth);
   const left = slidesToShow % 2 === 0 ? evenLeft : oddLeft;
+
+  // ===== track and increment current slide index =====
+  const [currSlide, setCurrSlide] = useState(1);
+  const incrementCurrSlide = ({ prevNext, safeSlidesToScroll }) => {
+    let newCurrSlideIndex;
+    if (prevNext === "prev") {
+      if (currSlide >= safeSlidesToScroll) {
+        setCurrSlide(currSlide - safeSlidesToScroll);
+      } else {
+        setCurrSlide(0);
+      }
+    } else {
+      if (currSlide <= slideCount - safeSlidesToScroll) {
+        setCurrSlide(currSlide + safeSlidesToScroll);
+      } else {
+        setCurrSlide(slideCount - 1);
+      }
+    }
+  };
+
   return (
-    <div className="window" ref={sliderRef}>
-      <div className="slides-container" style={{ left: left }}>
-        {slidesValidated ? (
-          slidesDataArray.map((slide, i) => (
-            <div className="slide-wrap" style={{ position: 'relative', width: `${slideWidth} !important` }}>
-              <SlideComponent                
-                slide={slide}
-                width={slideWidth}
-                key={`${slide}-${i}`}
-              />
-            </div>
-          ))
-        ) : (
-          <p>data for slides needs to be a non-empty array</p>
-        )}
+    <div className="wrap" ref={wrapRef}>
+      <button
+        onClick={() => incrementCurrSlide("prev", safeSlidesToScroll)}
+        className="prev prevNext"
+        style={{ width: prevNextButtonWidth }}>
+        <img src={arrowImageSrc} alt="left arrow" />
+      </button>
+      <div className="window" ref={windowRef}>
+        <div className="slides-container" style={{ left: left, width: (slideWidth * slideCount) }}>
+          {slidesValidated ? (
+            slidesDataArray.map((slide, i) => (
+              <div
+                className="slide-wrap"
+                style={{
+                  width: slideWidth,
+                  position: "relative",                  
+                }}
+                key={`${slide}-${i}`}>
+                <SlideComponent
+                  slide={slide}
+                  width={slideWidth}                  
+                />
+              </div>
+            ))
+          ) : (
+            <p>data for slides needs to be a non-empty array</p>
+          )}
+        </div>
       </div>
+      <button
+        onClick={() => incrementCurrSlide("next", safeSlidesToScroll)}
+        className="next prevNext"
+        style={{ width: prevNextButtonWidth }}>
+        <img src={arrowImageSrc} alt="right arrow" />
+      </button>
     </div>
   );
 };
@@ -56,13 +108,14 @@ export default Slider;
 Slider.propTypes = {
   settings: PropTypes.object,
   SlideComponent: PropTypes.func,
-  slidesDataArray: PropTypes.array
+  slidesDataArray: PropTypes.array,
+  arrowImageSrc: PropTypes.string
 };
 
 Slider.defaultProps = {
   settings: {
+    arrows: true,
     dots: true,
-    infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1
@@ -109,5 +162,7 @@ Slider.defaultProps = {
       title: "slide 3",
       copy: "lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet"
     }
-  ]
+  ],
+  arrowImageSrc:
+    "https://images.ctfassets.net/l6o0o2yu98mw/wqq1haDungEQedVzuTubI/38f9d68aa1495acbe74a78cfb09bf490/left_arrow.png?h=250"
 };
